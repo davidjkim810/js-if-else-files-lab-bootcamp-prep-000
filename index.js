b27a98e2337391b6da7657cf42c3955a00bc48b2
davidjkim810 <davidjkim810@gmail.com>
2017-08-16 17:04:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var name = "Joe"
+var name = "Susan"
 var height = "74"
 
 if (name === "Susan") {
@@ -6,6 +6,7 @@ if (name === "Susan") {
 } else {
     console.log("Expected the name to be Susan")
 }
+
 
 
 // Don't worry about this
